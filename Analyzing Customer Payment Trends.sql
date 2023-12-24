ca73997f245a8f2b2da1d47e05d289563e6b1d29
ranjanshivam1 <132743857+ranjanshivam1@users.noreply.github.com>
2023-12-24 15:12:34 +0530

    Add files via upload

---
+++ Analyzing Customer Payment Trends.sql
@@ -0,0 +1,13 @@
+SELECT
+c.customerNumber,
+c.customerName,
+
+p.paymentDate,
+p.amount AS paymentAmount
+FROM
+
+mintclassics.customers AS c
+LEFT JOIN
+mintclassics.payments AS p ON c.customerNumber = p.customerNumber
+order by
+paymentAmount desc
